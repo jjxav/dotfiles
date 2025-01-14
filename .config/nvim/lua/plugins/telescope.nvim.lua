f02fc729553931f2f0e8ba2bd4a3767cdd00e817
Xavier Legault <legault.xavier@live.ca>
2025-01-14 18:46:31 +0000

    Add git file search

--- .config/nvim/lua/plugins/telescope.nvim.lua
+++ .config/nvim/lua/plugins/telescope.nvim.lua
@@ -12,6 +12,7 @@ return {
 
 		vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
 		vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+		vim.keymap.set('n', '<leader>gf', builtin.git_files, { desc = 'Search [G]it [F]iles' })
 		vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
 		vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
 		vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
