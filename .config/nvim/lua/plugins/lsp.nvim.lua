ec040c08e9e50766d790327fd806e3ada9c4123e
Xavier Legault <legault.xavier@live.ca>
2025-01-14 19:07:12 +0000

    Add more default lsp

--- .config/nvim/lua/plugins/lsp.nvim.lua
+++ .config/nvim/lua/plugins/lsp.nvim.lua
@@ -1,9 +1,45 @@
 
 -- TODO: Add more server
 local servers = {
-	lua_ls = {
+	clangd = {},
+	pyright = {},
+	html = { filetypes = { 'html', 'twig', 'hbs'} },
+	angularls = { filetypes = { 'angular' } },
+	intelephense = {
+		stubs = {
+			"amqp", "apache", "apcu", "bcmath", "blackfire", "bz2", "calendar", "cassandra",
+			"com_dotnet", "Core", "couchbase", "crypto", "ctype", "cubrid", "curl", "date",
+			"dba", "decimal", "dom", "ds", "enchant", "Ev", "event", "exif", "fann", "FFI",
+			"ffmpeg", "fileinfo", "filter", "fpm", "ftp", "gd", "gearman", "geoip", "geos",
+			"gettext", "gmagick", "gmp", "gnupg", "grpc", "hash", "http", "ibm_db2", "iconv",
+			"igbinary", "imagick", "imap", "inotify", "interbase", "intl", "json", "judy",
+			"ldap", "leveldb", "libevent", "libsodium", "libxml", "lua", "lzf", "mailparse",
+			"mapscript", "mbstring", "mcrypt", "memcache", "memcached", "meminfo", "meta",
+			"ming", "mongo", "mongodb", "mosquitto-php", "mqseries", "msgpack", "mssql", "mysql",
+			"mysql_xdevapi", "mysqli", "ncurses", "newrelic", "oauth", "oci8", "odbc", "openssl",
+			"parallel", "Parle", "pcntl", "pcov", "pcre", "pdflib", "PDO", "pdo_ibm", "pdo_mysql",
+			"pdo_pgsql", "pdo_sqlite", "pgsql", "Phar", "phpdbg", "posix", "pspell", "pthreads",
+			"radius", "rar", "rdkafka", "readline", "recode", "redis", "Reflection", "regex",
+			"rpminfo", "rrd", "SaxonC", "session", "shmop", "SimpleXML", "snmp", "soap",
+			"sockets", "sodium", "solr", "SPL", "SplType", "SQLite", "sqlite3", "sqlsrv",
+			"ssh2", "standard", "stats", "stomp", "suhosin", "superglobals", "svn", "sybase",
+			"sync", "sysvmsg", "sysvsem", "sysvshm", "tidy", "tokenizer", "uopz", "uv", "v8js",
+			"wddx", "win32service", "winbinder", "wincache", "wordpress", "xcache", "xdebug",
+			"xhprof", "xml", "xmlreader", "xmlrpc", "xmlwriter", "xsl", "xxtea", "yaf", "yaml",
+			"yar", "zend", "Zend OPcache", "ZendCache", "ZendDebugger", "ZendUtils", "zip",
+			"zlib", "zmq", "zookeeper"
+		},
+		environment = { phpVersion = '8.4.0' }
+	},
 
-	}
+	lua_ls = {
+		Lua = {
+			workspace = { checkThirdParty = false },
+			telemetry = { enable = false },
+			-- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
+			diagnostics = { disable = { 'missing-fields' } },
+		},
+	},
 }
 
 return {
