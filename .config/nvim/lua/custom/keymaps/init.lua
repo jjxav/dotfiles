2b56965610d227aee9bb358ce6592272b411564c
Xavier Legault <legault.xavier@live.ca>
2025-01-15 02:02:22 +0000

    Remove old custom file

--- .config/nvim/lua/custom/keymaps/init.lua
+++
@@ -1,7 +0,0 @@
-vim.keymap.set('n', '<leader>fp', vim.cmd.bprevious, { desc = 'Go to previous buffer' })
-vim.keymap.set('n', '<leader>fn', vim.cmd.bnext, { desc = 'Go to next buffer' })
-
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-
-vim.keymap.set('v', '<leader>p', '"_dP')
