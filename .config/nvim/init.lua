93636422d42e02c913ac11fd40e998d6cfecdcdd
Xavier Legault <legault.xavier@live.ca>
2025-01-20 11:02:09 +0000

    Change tmux leader key

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -10,7 +10,6 @@ require("keymap")
 vim.wo.number = true
 vim.wo.relativenumber = true
 
--- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
@@ -39,14 +38,9 @@ vim.o.foldenable = false
 
 vim.wo.wrap = false
 
--- Sets how neovim will display certain whitespace characters in the editor.
---  See `:help 'list'`
---  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
 
--- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
--- Show which line your cursor is on
 vim.opt.cursorline = true
