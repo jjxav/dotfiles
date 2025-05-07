7ce96d60b2c0deb9410d73acdffc02212e861b67
Xavier Legault <legault.xavier@live.ca>
2025-05-07 09:52:20 -0400

    Note what need to be added

--- install.sh
+++ install.sh
@@ -1,11 +1,29 @@
 
 sudo apt install -y stow \
+	unzip \
+	zip \
 	ripgrep \
 	bat \
 	tmux \
 	zsh \
-	exa \
-	jq
+	jq \
+	eza \
+	lazygit
+	clang
 
 curl -s https://ohmyposh.dev/install.sh | bash -s
+
+#curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+#sudo rm -rf /opt/nvim
+#sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# -[x] install go
+# -[x] install nvm
+# -[x] install lazygit
+# -[x] install tmux tpm
+# -[x] install fzf
+# -[ ] zoxide
+
 
