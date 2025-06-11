61d12ac422ab33e9ecb381323f7cd010efbc2f3b
Xavier Legault <legault.xavier@live.ca>
2025-06-11 08:55:46 -0400

    Set markdown style

--- .config/nvim/lua/plugins/markdown.nvim.lua
+++ .config/nvim/lua/plugins/markdown.nvim.lua
@@ -1,6 +1,22 @@
 return {
 	'MeanderingProgrammer/render-markdown.nvim',
     dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
-    opts = {},
+    opts = {
+		-- preset = 'lazy'
+		heading = {
+			-- position = 'inline',
+			width = { 'full', 'full', 'block', 'block', 'block', 'block' },
+			border = true,
+			border_virtual = true,
+		},
+		indent = {
+			enabled = true,
+			skip_heading = true,
+			icon = ' ',
+		},
+		code = {
+			border = 'thick',
+		}
+	},
 
 }
