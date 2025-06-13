da76b903c84145fd1c93c1a1c4c2a12f85a60d7c
Xavier Legault <legault.xavier@live.ca>
2025-06-13 08:14:44 -0400

    Add zoxide & glab install

--- install.sh
+++ install.sh
@@ -19,15 +19,19 @@ curl -s https://ohmyposh.dev/install.sh | bash -s
 
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-mkdir -p ./gits/delta
 wget -O ./gits/delta.deb https://github.com/dandavison/delta/releases/download/0.18.2/git-delta_0.18.2_amd64.deb
 sudo dpkg -i ./gits/delta.deb
+
+wget -O ./gits/glab.deb https://gitlab.com/gitlab-org/cli/-/releases/v1.59.2/downloads/glab_1.59.2_linux_amd64.deb
+sudo dpkg -i ./gits/glab.deb
+
+wget -O ./gits/zoxide.deb https://github.com/ajeetdsouza/zoxide/releases/download/v0.9.8/zoxide_0.9.8-1_amd64.deb
+sudo dpkg -i ./gits/zoxide.deb
 
 # -[x] install go
 # -[x] install nvm
 # -[x] install lazygit
 # -[x] install tmux tpm
 # -[x] install fzf
-# -[ ] zoxide
-
+# -[x] zoxide
 
