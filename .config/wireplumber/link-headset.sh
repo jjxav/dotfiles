ccf2e36f0e4b4deca4fb9f5eb9eaa5db137a44e2
Xavier Legault <legault.xavier@live.ca>
2025-07-15 18:50:25 -0400

    Start of script sound control

--- .config/wireplumber/link-headset.sh
+++ .config/wireplumber/link-headset.sh
@@ -3,7 +3,25 @@
 #
 #
 
-wpctl status --name | grep 'alsa.output' | grep -Eo '\. \S*' | cut -c3-
+# wpctl status --name | grep 'alsa.output' | grep -Eo '\. \S*' | cut -c3-
+
+
+desktop_group=$(pw-dump | jq '.[] | select( .type | contains("PipeWire:Interface:Node") ) | select( .info.props["node.group"] == "Desktop" )' | jq -s)
+
+
+desktop_output=$(echo $desktop_group | jq '.[] | select( .info.props["custom.output.out"] )')
+desktop_output_id=$(echo $desktop_output | jq -r '.id')
+
+link=$(pw-dump | jq '.[] | select( .type | contains("PipeWire:Interface:Node") ) | select( .info.props["node.group"] == "Desktop" )' | jq -s)
+
+
+echo $desktop_output_id
+
+# PipeWire:Interface:Node => Mics Output (in and out), Desktop output (in and out), Device Node ()
+# PipeWire:Interface:Port => LF and RH
+# PipeWire:Interface:Device => Physical Decive
+#
+# PipeWire:Interface:Link => Data Structure
 
 
 # wait a second for the drivers to load
@@ -14,4 +32,10 @@ wpctl status --name | grep 'alsa.output' | grep -Eo '\. \S*' | cut -c3-
 # sudo -u user1 env XDG_RUNTIME_DIR=/run/user/1000 pw-link "Desktop:monitor_FR" alsa_output.usb-Kingston_HyperX_Cloud_Flight_S_000000000001-00.analog-stereo:playback_FR
 #
 # # finish and return the code for success
-# exit 0
+# exit 1
+
+
+
+# List device to up the sound level
+# WIP: .[] | select( .type | contains("PipeWire:Interface:Device") ) | select ( .info.props["media.class"] | contains("Audio/Device") ) #| select( .info.props["device.disabled"]  ) #| .info.props
+#
