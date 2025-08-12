419662c51bdeb31ee27f39207a0713cdd8269eee
Xavier Legault <legault.xavier@live.ca>
2025-08-12 18:00:26 -0400

    Add presenting plugins to nvim

---
+++ .config/nvim/lua/plugins/presenting.nvim.lua
@@ -0,0 +1,12 @@
+return {
+	"sotte/presenting.nvim",
+	opts = {
+		-- fill in your options here
+		-- see :help Presenting.config
+		options = {
+			-- The width of the slide buffer.
+			width = 120,
+		},
+	},
+	cmd = { "Presenting" },
+}
