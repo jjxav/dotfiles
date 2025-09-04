3224a3e7897cc8494308c1b07154430fcfa8f2e7
Xavier Legault <legault.xavier@live.ca>
2025-09-04 08:38:21 -0400

    feat: Add typst config to my nvim config

---
+++ .config/nvim/lua/plugins/typst.nvim.lua
@@ -0,0 +1,35 @@
+return {
+	'chomosuke/typst-preview.nvim',
+	lazy = false, -- or ft = 'typst'
+	version = '1.*',
+	config = function()
+		local typstp = require('typst-preview').setup {
+			open_cmd = "echo '' > /dev/null",
+			port = 3000,
+		}
+
+		local close_preview = function(buff_id)
+			local cmd = vim.api.nvim_parse_cmd('TypstPreviewStop', {})
+			vim.api.nvim_cmd(cmd, {})
+		end
+		local open_preview = function(buff_id)
+			local cmd = vim.api.nvim_parse_cmd('TypstPreview', {})
+			vim.api.nvim_cmd(cmd, {})
+		end
+
+		vim.api.nvim_create_autocmd('BufEnter', {
+			pattern = { '*.typ', '*.typst' },
+			group = vim.api.nvim_create_augroup('typst-attach', { clear = true }),
+			callback = function(event)
+				open_preview(event.buf)
+			end
+		})
+		vim.api.nvim_create_autocmd('BufLeave', {
+			pattern = { '*.typ', '*.typst' },
+			group = vim.api.nvim_create_augroup('typst-detattach', { clear = true }),
+			callback = function(event)
+				close_preview(event.buf)
+			end
+		})
+	end
+}
