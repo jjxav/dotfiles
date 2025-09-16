0c1d1d4871c51eda6bef01d67f7df244ed9db4db
Xavier Legault <legault.xavier@live.ca>
2025-09-16 19:36:21 -0400

    feat: Update harpoon config

--- .config/nvim/lua/plugins/harpoon.nvim.lua
+++ .config/nvim/lua/plugins/harpoon.nvim.lua
@@ -6,11 +6,16 @@ return {
 		local harpoon = require("harpoon")
 		local set = require('jjxav.helper').keymap.set
 
-		set("<leader>ya", function() harpoon:list():add() end, "[A]ppend")
-		set("<leader>yd", function() harpoon:list():remove() end, "[D]elete")
-		set("<leader>yc", function() harpoon:list():clear() end, "[C]lear")
+		set("<A-a>", function() harpoon:list():add() end, "[A]ppend")
+		set("<A-d>", function() harpoon:list():remove() end, "[D]elete")
+		set("<A-c>", function() harpoon:list():clear() end, "[C]lear")
 
 		set("<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+		set("<A-q>", function() harpoon:list():select(1) end)
+		set("<A-w>", function() harpoon:list():select(2) end)
+		set("<A-e>", function() harpoon:list():select(3) end)
+		set("<A-r>", function() harpoon:list():select(4) end)
 
 		-- Toggle previous & next buffers stored within Harpoon list
 		set("<A-n>", function() harpoon:list():prev() end)
