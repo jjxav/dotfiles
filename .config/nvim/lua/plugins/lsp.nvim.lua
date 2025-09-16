534a757096236781343034e15f1bbf7516496a85
Xavier Legault <legault.xavier@live.ca>
2025-09-16 11:25:02 -0400

    fix: Stop infinit redeclaration of the lsp register handler function
    The register handler function was overrightend each time the LspAttach
    event was triggered which in turn made each call call the previously
    overighten function

--- .config/nvim/lua/plugins/lsp.nvim.lua
+++ .config/nvim/lua/plugins/lsp.nvim.lua
@@ -64,127 +64,130 @@ return {
 			vim.keymap.del('n', 'gri')
 			vim.keymap.del('n', 'grt')
 
+			local builtin = require('telescope.builtin')
+
+
+			local register = function(client, bufnr)
+				local set = function(keys, func, desc, mode)
+					local _set = require('jjxav.helper').keymap.set
+					_set(keys, func, 'LSP: ' .. desc, mode, { buffer = bufnr })
+				end
+
+				if not client then
+					return
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_formatting, bufnr) then
+					vim.api.nvim_buf_create_user_command(
+						bufnr,
+						'Format',
+						function() vim.lsp.buf.format({ buffer = bufnr }) end,
+						{}
+					)
+					set('<leader>ff', vim.lsp.buf.format, '[F]ile [F]ormat')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_rangeFormatting, bufnr) then
+					set('<leader>ff', vim.lsp.buf.format, '[F]ile Range [F]ormat', 'v')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, bufnr) then
+					local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
+
+					vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+						buffer = bufnr,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.document_highlight,
+					})
+					vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+						buffer = bufnr,
+						group = highlight_augroup,
+						callback = vim.lsp.buf.clear_references,
+					})
+
+					vim.api.nvim_create_autocmd('LspDetach', {
+						group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
+						callback = function(event2)
+							vim.lsp.buf.clear_references()
+							vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
+						end,
+					})
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
+					set(
+						'<leader>th',
+						function()
+							vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = bufnr })
+						end,
+						'[T]oggle Inlay [H]ints'
+					)
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_foldingRange, bufnr) then
+					vim.o.foldmethod = 'expr'
+					vim.o.foldexpr = "nvim_treesitter#foldexpr()"
+				else
+					vim.o.foldmethod = 'manual'
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_rename, bufnr) then
+					set('<leader>cr', vim.lsp.buf.rename, '[C]ursor [R]ename')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_codeAction, bufnr) then
+					set('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.workspace_symbol, bufnr) then
+					set('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentSymbol, bufnr) then
+					set('<leader>sd', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_definition, bufnr) then
+					set('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_typeDefinition, bufnr) then
+					set('<leader>D', builtin.lsp_type_definitions, 'Type [D]efinition')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_declaration, bufnr) then
+					set('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_implementation, bufnr) then
+					set('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_references, bufnr) then
+					set('gr', builtin.lsp_references, '[G]oto [R]eferences')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_diagnostic, bufnr) then
+					set('<leader>D', builtin.diagnostics, '[S]earch [D]iagnostics')
+				end
+
+				if client:supports_method(vim.lsp.protocol.Methods.textDocument_hover, bufnr) then
+					set('K', function()
+						vim.lsp.buf.hover({
+							close_events = { 'CursorMoved', 'CursorMovedI', 'InsertCharPre', 'BufHidden' }
+						})
+					end, '[S]earch [D]iagnostics')
+				end
+
+				-- maybe textDocument_inlayHint, textDocument_signatureHelp
+			end
+
+
 			vim.api.nvim_create_autocmd('LspAttach', {
 				group = vim.api.nvim_create_augroup('lsp-attach', { clear = true }),
 				nested = true,
 				callback = function(event)
-					local builtin = require('telescope.builtin')
 					-- local lsp_builtin = vim.lsp.buf
-
-					local set = function(keys, func, desc, mode)
-						local _set = require('jjxav.helper').keymap.set
-						_set(keys, func, 'LSP: ' .. desc, mode, { buffer = event.buf })
-					end
-
-					local register = function(client, bufnr)
-						if not client then
-							return
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_formatting, bufnr) then
-							vim.api.nvim_buf_create_user_command(
-								bufnr,
-								'Format',
-								function() vim.lsp.buf.format({ buffer = bufnr }) end,
-								{}
-							)
-							set('<leader>ff', vim.lsp.buf.format, '[F]ile [F]ormat')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_rangeFormatting, bufnr) then
-							set('<leader>ff', vim.lsp.buf.format, '[F]ile Range [F]ormat', 'v')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, bufnr) then
-							local highlight_augroup = vim.api.nvim_create_augroup('lsp-highlight', { clear = false })
-
-							vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
-								buffer = event.buf,
-								group = highlight_augroup,
-								callback = vim.lsp.buf.document_highlight,
-							})
-							vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
-								buffer = event.buf,
-								group = highlight_augroup,
-								callback = vim.lsp.buf.clear_references,
-							})
-
-							vim.api.nvim_create_autocmd('LspDetach', {
-								group = vim.api.nvim_create_augroup('lsp-detach', { clear = true }),
-								callback = function(event2)
-									vim.lsp.buf.clear_references()
-									vim.api.nvim_clear_autocmds { group = 'lsp-highlight', buffer = event2.buf }
-								end,
-							})
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, bufnr) then
-							set(
-								'<leader>th',
-								function()
-									vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
-								end,
-								'[T]oggle Inlay [H]ints'
-							)
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_foldingRange, bufnr) then
-							vim.o.foldmethod = 'expr'
-							vim.o.foldexpr = "nvim_treesitter#foldexpr()"
-						else
-							vim.o.foldmethod = 'manual'
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_rename, bufnr) then
-							set('<leader>cr', vim.lsp.buf.rename, '[C]ursor [R]ename')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_codeAction, bufnr) then
-							set('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.workspace_symbol, bufnr) then
-							set('<leader>ws', builtin.lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_documentSymbol, bufnr) then
-							set('<leader>sd', builtin.lsp_document_symbols, '[D]ocument [S]ymbols')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_definition, bufnr) then
-							set('gd', builtin.lsp_definitions, '[G]oto [D]efinition')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_typeDefinition, bufnr) then
-							set('<leader>D', builtin.lsp_type_definitions, 'Type [D]efinition')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_declaration, bufnr) then
-							set('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_implementation, bufnr) then
-							set('gI', builtin.lsp_implementations, '[G]oto [I]mplementation')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_references, bufnr) then
-							set('gr', builtin.lsp_references, '[G]oto [R]eferences')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_diagnostic, bufnr) then
-							set('<leader>D', builtin.diagnostics, '[S]earch [D]iagnostics')
-						end
-
-						if client:supports_method(vim.lsp.protocol.Methods.textDocument_hover, bufnr) then
-							set('K', function()
-								vim.lsp.buf.hover({
-									close_events = { 'CursorMoved', 'CursorMovedI', 'InsertCharPre', 'BufHidden' }
-								})
-							end, '[S]earch [D]iagnostics')
-						end
-
-						-- maybe textDocument_inlayHint, textDocument_signatureHelp
-					end
 
 					-- Mapping to remove to be mapped else where
 					--[[
@@ -202,23 +205,23 @@ return {
 					if client then
 						register(client, event.buf)
 					end
-
-					-- Add a handler for when a LSP add capability after attaching
-					vim.lsp.handlers['client/registerCapability'] = (function(overridden)
-						return function(err, res, ctx)
-							local result = overridden(err, res, ctx)
-							local handler_client = vim.lsp.get_client_by_id(ctx.client_id)
-							if not handler_client then
-								return
-							end
-							for bufnr, _ in pairs(handler_client.attached_buffers) do
-								register(handler_client, bufnr)
-							end
-							return result
-						end
-					end)(vim.lsp.handlers['client/registerCapability'])
 				end,
 			})
+
+			-- Add a handler for when a LSP add capability after attaching
+			vim.lsp.handlers['client/registerCapability'] = (function(overridden)
+				return function(err, res, ctx)
+					local result = overridden(err, res, ctx)
+					local handler_client = vim.lsp.get_client_by_id(ctx.client_id)
+					if not handler_client then
+						return
+					end
+					for bufnr, _ in pairs(handler_client.attached_buffers) do
+						register(handler_client, bufnr)
+					end
+					return result
+				end
+			end)(vim.lsp.handlers['client/registerCapability'])
 
 			-- vim.api.nvim_create_autocmd('LspNotify', {
 			-- 	callback = function(args)
