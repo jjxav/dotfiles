a0e7f5797267fc49106fe602b2126a3c97cb0d18
Xavier Legault <legault.xavier@live.ca>
2025-09-16 21:35:00 -0400

    feat: Refactor

--- .config/nvim/lua/plugins/gitsigns.nvim.lua
+++ .config/nvim/lua/plugins/gitsigns.nvim.lua
@@ -12,12 +12,6 @@ return {
 		on_attach = function(bufnr)
 			local gitsigns = require('gitsigns')
 
-			local function map(mode, l, r, opts)
-				opts = opts or {}
-				opts.buffer = bufnr
-				vim.keymap.set(mode, l, r, opts)
-			end
-
 			local set = require('jjxav.helper').keymap.set
 
 			-- Navigation
--- .config/nvim/lua/plugins/todo-comments.nvim.lua
+++ .config/nvim/lua/plugins/todo-comments.nvim.lua
@@ -1,9 +1,6 @@
 return {
 	{
 		'numToStr/Comment.nvim',
-		opts = {
-
-		}
 	},
 	{
 		'folke/todo-comments.nvim',
