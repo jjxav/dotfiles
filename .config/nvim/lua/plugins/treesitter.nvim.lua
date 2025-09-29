9e1f5475c89d8beca3c04d8c995ab647f5302798
Xavier Legault <legault.xavier@live.ca>
2025-09-29 06:19:39 -0400

    feat: Add autocmd to start treesitter

--- .config/nvim/lua/plugins/treesitter.nvim.lua
+++ .config/nvim/lua/plugins/treesitter.nvim.lua
@@ -4,6 +4,21 @@ return {
 		lazy = false,
 		branch = 'main',
 		build = ":TSUpdate",
+		config = function()
+			require 'nvim-treesitter'.setup()
+
+			vim.api.nvim_create_autocmd('FileType', {
+				callback = function()
+					local parser = vim.treesitter.get_parser(nil, nil, {
+						error = false,
+					})
+
+					if parser and vim.treesitter.start then
+						vim.treesitter.start()
+					end
+				end
+			})
+		end
 		-- opts = {
 		-- 	ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "html", "angular", "php" },
 		-- 	auto_install = true,
