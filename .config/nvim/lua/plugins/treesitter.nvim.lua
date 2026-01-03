70a61512a7105cae93acfe55dc09ccb336272bfb
Xavier Legault <legault.xavier@live.ca>
2026-01-03 14:59:17 -0500

    feat: Treesitter working config

--- .config/nvim/lua/plugins/treesitter.nvim.lua
+++ .config/nvim/lua/plugins/treesitter.nvim.lua
@@ -1,45 +1,56 @@
 return {
-	-- {
-	-- 	"nvim-treesitter/nvim-treesitter",
-	-- 	lazy = false,
-	-- 	enabled = true,
-	-- 	branch = 'main',
-	-- 	build = ":TSUpdate",
-	-- 	config = function()
-	-- 		require 'nvim-treesitter'.setup()
-	--
-	-- 		vim.api.nvim_create_autocmd('FileType', {
-	-- 			callback = function()
-	-- 				local parser = vim.treesitter.get_parser(nil, nil, {
-	-- 					error = false,
-	-- 				})
-	--
-	-- 				if parser and vim.treesitter.start then
-	-- 					vim.treesitter.start()
-	-- 				end
-	-- 			end
-	-- 		})
-	-- 	end
-	-- },
 	{
-		"nvim-treesitter/nvim-treesitter",
+		'nvim-treesitter/nvim-treesitter',
+		lazy = false,
 		enabled = true,
-		build = ":TSUpdate",
-		opts = {
-			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "html", "angular", "php" },
-			auto_install = true,
-			highlight = {
-				enable = true,
-			},
-			indent = {
-				enable = true,
+		branch = 'main',
+		build = ':TSUpdate',
+		config = function()
+			require('nvim-treesitter').setup()
+
+			require('nvim-treesitter').install {
+				'angular',
+				'bash',
+				'c',
+				'cpp',
+				'go',
+				'html',
+				'javascript',
+				'json',
+				'jsx',
+				'lua',
+				'markdown',
+				'php',
+				'python',
+				'rust',
+				'typescript',
 			}
-		},
+
+			local tree = require 'nvim-treesitter'
+
+			vim.api.nvim_create_autocmd('FileType', {
+				callback = function(arg)
+					local type = arg.match
+
+					local installed = tree.get_installed()
+					local treesitter_ready = vim.tbl_contains(installed, type)
+
+					if treesitter_ready and vim.treesitter.start then
+						local win_id = vim.api.nvim_get_current_win()
+
+						vim.treesitter.start(arg.buf, type)
+						vim.bo[arg.buf].syntax = 'on'
+						vim.wo[win_id][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+						vim.wo[win_id][0].foldmethod = 'expr'
+					end
+				end,
+			})
+		end,
 	},
 	{
 		'nvim-treesitter/nvim-treesitter-context',
 		opts = {
-			enable = true
-		}
-	}
+			enable = true,
+		},
+	},
 }
